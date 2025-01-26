2b5cd4b68101e3d9018e5ef3162af9f94213627e
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-25 21:42:25 -0300

    fix

--- src/app/components/template/Pagina.tsx
+++ src/app/components/template/Pagina.tsx
@@ -1,15 +1,17 @@
 import Menu from "./Menu"
 
-export interface PaginaProps{
-    children: any
+export interface PaginaProps {
+    children: React.ReactNode 
     className?: string
 }
 
-export default function Pagina(props: PaginaProps){
+export default function Pagina(props: PaginaProps) {
     return(
         <div className="flex">
             <Menu />
-            <main className={`flex-1 p-7 ${props.className ?? ''}`}>{props.children}</main>
+            <main className={`flex-1 p-7 ${props.className ?? ''}`}>
+                {props.children}
+            </main>
         </div>
     )
 }
