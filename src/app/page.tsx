1a2ace1b8c475e10d253f7637fbb69518056d0d7
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-25 22:06:07 -0300

    agora vai?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { GetServerSideProps } from 'next';
 import Pagina from "./components/template/Pagina";
+import { client, connectDb } from '@/dbConnection';
 
-export default function Home() {
+
+export default function Home({ data }: { data: any }) {
   return (
-    <Pagina>Opa</Pagina>
+    <Pagina>
+      <h1>Opa</h1>
+      <p>Dados do banco de dados: {JSON.stringify(data)}</p>
+    </Pagina>
   );
 }
+
+// Função para buscar dados do banco de dados
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Conectar ao banco de dados
+  await connectDb();
+
+  // Exemplo de consulta ao banco de dados
+  const res = await client.query('SELECT * FROM sua_tabela');
+  const data = res.rows;
+
+  return {
+    props: { data }, // Passando os dados para a página
+  };
+};
