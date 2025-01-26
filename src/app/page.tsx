f20d03374db2fd86ff8e7120c60c5ca7dc85735e
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-25 22:12:35 -0300

    voltando no tempo ult do ekko

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-import { GetServerSideProps } from 'next';
 import Pagina from "./components/template/Pagina";
-import { client, connectDb } from '@/dbConnection';
 
-
-export default function Home({ data }: { data: any }) {
+export default function Home() {
   return (
-    <Pagina>
-      <h1>Opa</h1>
-      <p>Dados do banco de dados: {JSON.stringify(data)}</p>
-    </Pagina>
+    <Pagina>Opa</Pagina>
   );
 }
-
-// Função para buscar dados do banco de dados
-export const getServerSideProps: GetServerSideProps = async () => {
-  // Conectar ao banco de dados
-  await connectDb();
-
-  // Exemplo de consulta ao banco de dados
-  const res = await client.query('SELECT * FROM sua_tabela');
-  const data = res.rows;
-
-  return {
-    props: { data }, // Passando os dados para a página
-  };
-};
